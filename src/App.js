0e34617acbc1a84f4cfb5661af69bcca04779b0a
Che Ndumbi <che.ndumbi@gmail.com>
2020-09-20 10:51:53 -0400

    finished 74

--- src/App.js
+++ src/App.js
@@ -1,19 +1,14 @@
 import React from "react";
 import { Route } from "react-router-dom";
 import HomePage from "./pages/homepage/homepage.component";
+import ShopPage from "./pages/shop/shop.component";
 import "./App.css";
-
-const HatsPage = () => (
-  <div>
-    <h1>THIS IS THE HATS PAGE</h1>
-  </div>
-);
 
 function App() {
   return (
     <div>
       <Route exact path="/" component={HomePage} />
-      <Route exact path="/hats" component={HatsPage} />
+      <Route exact path="/shop" component={ShopPage} />
     </div>
   );
 }
