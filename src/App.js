1c092db460a74e0a6334da134e732131866b4676
Che Ndumbi <che.ndumbi@gmail.com>
2020-09-29 10:56:35 -0400

    storing data in our App

--- src/App.js
+++ src/App.js
@@ -23,12 +23,24 @@ class App extends React.Component {
   unsubscribeFromAuth = null;
 
   componentDidMount() {
-    this.unsubscribeFromAuth = auth.onAuthStateChanged(async (user) => {
-      createUserProfileDocument(user);
-      this.setState({ currentUser: user });
-    });
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async (userAuth) => {
+      if (userAuth) {
+        const userRef = await createUserProfileDocument(userAuth);
 
-    createUserProfileDocument();
+        userRef.onSnapshot((snapShot) => {
+          console.log(snapShot.data());
+
+          this.setState({
+            currentUser: {
+              id: snapShot.id,
+              ...snapShot.data(),
+            },
+          });
+        });
+      }
+
+      this.setState({ currentUser: userAuth });
+    });
   }
 
   componentWillUnmount() {
