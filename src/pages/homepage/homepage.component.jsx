daff9cbdd6731ee5873ad46305fc555cffb25463
Che Ndumbi <che.ndumbi@gmail.com>
2020-10-08 12:37:35 -0400

    fixing mistakes

--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -1,7 +1,5 @@
 import React from "react";
-import MenuItem from "../../components/menu-item/menu-item.component";
 import Directory from "../../components/directory/directory.component";
-import NavBar from "../../components/header/header.component";
 
 import "./homepage.styles.scss";
 
